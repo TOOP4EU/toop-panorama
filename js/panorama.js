bb60a60f1618ea0fe92c84c06bd6b64868e8ddbc
Mary Petritsopoulou <mariap@dsv.su.se>
2018-04-27 09:00:29 +0200

    Object animation complete
    Completed the animation of the basic objects

--- js/panorama.js
+++ js/panorama.js
@@ -4,75 +4,93 @@ function items(offset) {
   console.log(offset.x); // Logs the current horizontal scroll offset
 
   if (offset.x >= 200) { spawnItem('smiley'); }
-  if (offset.x >= 1000) { spawnItem('AuthenticationMethOne'); }
-  if (offset.x >= 1000) { spawnItem('AuthenticationMethTwo'); }
-  if (offset.x >= 1000) { spawnItem('AuthenticationMethThree'); }
+  if (offset.x >= 1366) { spawnItem('AuthenticationMethOne'); }
+  if (offset.x >= 1366) { spawnItem('AuthenticationMethTwo'); }
+  if (offset.x >= 1366) { spawnItem('AuthenticationMethThree'); }
 
-  if (offset.x >= 2000) { spawnItem('eIDASNode'); }
-  if (offset.x >= 2000) { spawnItem('eIDASNodeOK'); }
-  if (offset.x >= 2000) { spawnItem('MaxsAuthenticated'); }
+  if (offset.x >= 2732) { spawnItem('eIDASNode'); }
+  if (offset.x >= 2732) { spawnItem('eIDASNodeOK'); }
+  if (offset.x >= 2732) { spawnItem('MaxsAuthenticated'); }
 
-  if (offset.x >= 3000) { spawnItem('GetCompanyInfo'); }
-  if (offset.x >= 3000) { spawnItem('GetCompanyInfoOk'); }
+  if (offset.x >= 4098) { spawnItem('GetCompanyInfo'); }
+  if (offset.x >= 4098) { spawnItem('GetCompanyInfoOk'); }
+  if (offset.x >= 4098) { spawnItem('Consent'); }
 
-  if (offset.x >= 4000) { spawnItem('Consent'); }
+  if (offset.x >= 5464) { spawnItem('TruckFreeBMOne'); }
+  if (offset.x >= 5464) { spawnItem('FreePackageOne'); }
+  if (offset.x >= 5464) { spawnItem('TruckTOOPBMOne'); }
+  if (offset.x >= 5464) { spawnItem('FreePackageTwo'); }
+  if (offset.x >= 5464) { spawnItem('SemanticMappingOne'); }
+  if (offset.x >= 5464) { spawnItem('TruckTOOPBMTwo'); }
+  if (offset.x >= 5464) { spawnItem('FreePackageThree'); }
+  if (offset.x >= 5464) { spawnItem('TOOPPackageOne'); }
 
-  if (offset.x >= 5000) { spawnItem('TruckFreeBMOne'); }
-  if (offset.x >= 5000) { spawnItem('FreePackageOne'); }
-  if (offset.x >= 5000) { spawnItem('TruckTOOPBMOne'); }
-  if (offset.x >= 5000) { spawnItem('FreePackageTwo'); }
-  if (offset.x >= 5000) { spawnItem('SemanticMappingOne'); }
-  if (offset.x >= 5000) { spawnItem('TruckTOOPBMTwo'); }
-  if (offset.x >= 5000) { spawnItem('FreePackageThree'); }
-  if (offset.x >= 5000) { spawnItem('TOOPPackageOne'); }
+  if (offset.x >= 6830) { spawnItem('DataDiscoveryOne'); }
+  if (offset.x >= 6830) { spawnItem('TruckTOOPBMThree'); }
+  if (offset.x >= 6830) { spawnItem('FreePackageFour'); }
+  if (offset.x >= 6830) { spawnItem('TOOPPackageTwo'); }
+  if (offset.x >= 6830) { spawnItem('TOOPPackageThree'); }
+  if (offset.x >= 6830) { spawnItem('BaloonOne'); }
+  if (offset.x >= 6830) { spawnItem('FreePackageFive'); }
+  if (offset.x >= 6830) { spawnItem('TOOPPackageFour'); }
+  if (offset.x >= 6830) { spawnItem('TOOPPackageFive'); }
 
-  if (offset.x >= 6000) { spawnItem('DataDiscoveryOne'); }
-  if (offset.x >= 6000) { spawnItem('TruckTOOPBMThree'); }
-  if (offset.x >= 6000) { spawnItem('FreePackageFour'); }
-  if (offset.x >= 6000) { spawnItem('TOOPPackageTwo'); }
-  if (offset.x >= 6000) { spawnItem('TOOPPackageThree'); }
-  if (offset.x >= 6000) { spawnItem('BaloonOne'); }
-  if (offset.x >= 6000) { spawnItem('FreePackageFive'); }
-  if (offset.x >= 6000) { spawnItem('TOOPPackageFour'); }
-  if (offset.x >= 6000) { spawnItem('TOOPPackageFive'); }
+  if (offset.x >= 8196) { spawnItem('TruckTOOPBMFour'); }
+  if (offset.x >= 8196) { spawnItem('FreePackageSix'); }
+  if (offset.x >= 8196) { spawnItem('TOOPPackageSix'); }
+  if (offset.x >= 8196) { spawnItem('TOOPPackageSeven'); }
+  if (offset.x >= 8196) { spawnItem('SemanticMappingTwo'); }
+  if (offset.x >= 8196) { spawnItem('TruckTOOPBMFive'); }
+  if (offset.x >= 8196) { spawnItem('FreePackageSeven'); }
+  if (offset.x >= 8196) { spawnItem('TOOPPackageEight'); }
+  if (offset.x >= 8196) { spawnItem('TOOPPackageNine'); }
+  if (offset.x >= 8196) { spawnItem('EloniaPackageOne'); }
 
-  if (offset.x >= 7000) { spawnItem('TruckTOOPBMFour'); }
-  if (offset.x >= 7000) { spawnItem('FreePackageSix'); }
-  if (offset.x >= 7000) { spawnItem('TOOPPackageSix'); }
-  if (offset.x >= 7000) { spawnItem('TOOPPackageSeven'); }
+  if (offset.x >= 9562) { spawnItem('TruckTOOPBMSix'); }
+  if (offset.x >= 9562) { spawnItem('FreePackageEight'); }
+  if (offset.x >= 9562) { spawnItem('TOOPPackageTen'); }
+  if (offset.x >= 9562) { spawnItem('TOOPPackageEleven'); }
+  if (offset.x >= 9562) { spawnItem('EloniaPackageTwo'); }
+  if (offset.x >= 9562) { spawnItem('TruckEloniaBMOne'); }
+  if (offset.x >= 9562) { spawnItem('FreePackageNine'); }
+  if (offset.x >= 9562) { spawnItem('TOOPPackageTwelve'); }
+  if (offset.x >= 9562) { spawnItem('EloniaPackageThree'); }
 
-  if (offset.x >= 8000) { spawnItem('SemanticMappingTwo'); }
-  if (offset.x >= 8000) { spawnItem('TruckTOOPBMFive'); }
-  if (offset.x >= 8000) { spawnItem('FreePackageSeven'); }
-  if (offset.x >= 8000) { spawnItem('TOOPPackageEight'); }
-  if (offset.x >= 8000) { spawnItem('TOOPPackageNine'); }
-  if (offset.x >= 8000) { spawnItem('EloniaPackageOne'); }
+  if (offset.x >= 10928) { spawnItem('TruckEloniaBMTwo'); }
+  if (offset.x >= 10928) { spawnItem('FreePackageTen'); }
+  if (offset.x >= 10928) { spawnItem('TOOPPackageThirteen'); }
+  if (offset.x >= 10928) { spawnItem('EloniaPackageFour'); }
+  if (offset.x >= 10928) { spawnItem('DataPackageOne'); }
+  if (offset.x >= 10928) { spawnItem('TruckTOOPBMSeven'); }
+  if (offset.x >= 10928) { spawnItem('FreePackageEleven'); }
+  if (offset.x >= 10928) { spawnItem('TOOPPackageFourteen'); }
+  if (offset.x >= 10928) { spawnItem('EloniaPackageFive'); }
+  if (offset.x >= 10928) { spawnItem('DataPackageTwo'); }
 
-  if (offset.x >= 9000) { spawnItem('TruckTOOPBMSix'); }
-  if (offset.x >= 9000) { spawnItem('FreePackageEight'); }
-  if (offset.x >= 9000) { spawnItem('TOOPPackageTen'); }
-  if (offset.x >= 9000) { spawnItem('TOOPPackageEleven'); }
-  if (offset.x >= 9000) { spawnItem('EloniaPackageTwo'); }
+  if (offset.x >= 12294) { spawnItem('DataDiscoveryTwo'); }
+  if (offset.x >= 12294) { spawnItem('BaloonTwo'); }
+  if (offset.x >= 12294) { spawnItem('FreePackageTwelve'); }
+  if (offset.x >= 12294) { spawnItem('TOOPPackageFifteen'); }
+  if (offset.x >= 12294) { spawnItem('TOOPPackageSixteen'); }
+  if (offset.x >= 12294) { spawnItem('EloniaPackageSix'); }
+  if (offset.x >= 12294) { spawnItem('DataPackageThree'); }
 
-  if (offset.x >= 9000) { spawnItem('TruckEloniaBMOne'); }
-  if (offset.x >= 9000) { spawnItem('FreePackageNine'); }
-  if (offset.x >= 9000) { spawnItem('TOOPPackageTwelve'); }
-  if (offset.x >= 9000) { spawnItem('EloniaPackageThree'); }
+  if (offset.x >= 13660) { spawnItem('TruckTOOPBMEight'); }
+  if (offset.x >= 13660) { spawnItem('FreePackageThirteen'); }
+  if (offset.x >= 13660) { spawnItem('TOOPPackageSixteen'); }
+  if (offset.x >= 13660) { spawnItem('TOOPPackageSeventeen'); }
+  if (offset.x >= 13660) { spawnItem('EloniaPackageSeven'); }
+  if (offset.x >= 13660) { spawnItem('DataPackageFour'); }
+  if (offset.x >= 13660) { spawnItem('TruckFreeBMTwo'); }
+  if (offset.x >= 13660) { spawnItem('FreePackageFourteen'); }
+  if (offset.x >= 13660) { spawnItem('DataPackageFive'); }
+  if (offset.x >= 13660) { spawnItem('RegWEEEOne'); }
 
-  if (offset.x >= 10000) { spawnItem('TruckEloniaBMTwo'); }
-  if (offset.x >= 10000) { spawnItem('FreePackageTen'); }
-  if (offset.x >= 10000) { spawnItem('TOOPPackageThirteen'); }
-  if (offset.x >= 10000) { spawnItem('EloniaPackageFour'); }
-  if (offset.x >= 10000) { spawnItem('DataPackageOne'); }
+  if (offset.x >= 15026) { spawnItem('RegWEEETwo'); }
+  if (offset.x >= 15026) { spawnItem('SubmissionScr'); }
 
-  if (offset.x >= 10000) { spawnItem('TruckTOOPBMSeven'); }
-  if (offset.x >= 10000) { spawnItem('FreePackageEleven'); }
-  if (offset.x >= 10000) { spawnItem('TOOPPackageFourteen'); }
-  if (offset.x >= 10000) { spawnItem('EloniaPackageFive'); }
-  if (offset.x >= 10000) { spawnItem('DataPackageTwo'); }
+  if (offset.x >= 16392) { spawnItem('SuccessScr'); }
 
-  if (offset.x >= 11000) { spawnItem('BaloonTwo'); }
-  if (offset.x >= 11000) { spawnItem('FreePackageTwelve'); }
 }
 
 function panorama(content) {
